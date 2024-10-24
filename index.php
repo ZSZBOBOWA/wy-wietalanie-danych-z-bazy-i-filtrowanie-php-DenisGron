6bfb49854a79496faf1809b57cf17cb541275d4b
Denis Gron <EMAIL>
2024-10-24 08:59:08 +0200

    Modyfikacja Kodu PHP do Filtrowania

--- index.php
+++ index.php
@@ -8,26 +8,34 @@
 <body>
     <form method="POST" action="index.php">
         Wpisz nazwisko: <input type="text" name="nazwisko">
-        <input type="submit" value="Szukaj">
+        <input type="submit" value="Filtruj">
 </form>
     <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $conn = mysqli_connect($servername, $username, $password);
+    $dbname = "szkola";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     if (!$conn) {
         die ("Błąd połączenia: " . mysqli_connect_error());
     }
-    ?>
-    <?php
-    $sql = "SELECT * FROM uczniowie";
+
+    if(isset($_POST['nazwisko']) && $_POST['nazwisko'] != ''){
+        $nazwisko = $_POST['nazwisko'];
+        $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
+
+        $sql = "SELECT * FROM uczniowie WHERE nazwisko='$nazwisko'";
+    } else {
+        $sql = "SELECT * FROM uczniowie";
+    }
+
     $result = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
         echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" .$row|["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" .$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
         }
         echo "</table>";
     } else {
@@ -35,5 +43,6 @@
     }
     mysqli_close($conn);
     ?>
+
 </body>
 </html>

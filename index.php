c1f11735874ae67da6cb57a37978700bc520674f
Denis Gron <EMAIL>
2024-10-24 08:37:55 +0200

    Pobieranie i Wyświetlanie Danych

--- index.php
+++ index.php
@@ -18,5 +18,21 @@
         die ("Błąd połączenia: " . mysqli_connect_error());
     }
     ?>
+
+    <?php
+    $sql = "SELECT * FROM uczniowie";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0) {
+        echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" .$row|["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Brak wyników";
+    }
+    mysqli_close($conn);
+    ?>
 </body>
 </html>
